0897089cdf08acf09dcddf7af963005c252e0493
ampueropierre <ampuero.pierre@gmail.com>
2018-05-10 12:17:49 +0200

    Create Heroku

--- Landing_page/config/routes.rb
+++ Landing_page/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/hackers', to: 'home#hackers'
+  get 'home/pros', to: 'home#pros'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
