5a83bc68115719ace291bb1e78aed7035cdcbae8
ampueropierre <ampuero.pierre@gmail.com>
2018-05-10 19:35:58 +0200

    root

--- Landing_page/config/routes.rb
+++ Landing_page/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/hackers', to: 'home#hackers'
-  get 'home/pros', to: 'home#pros'
+	root to: 'home#pros'
+  get '/hackers', to: 'home#hackers'
+  get '/pros', to: 'home#pros'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
