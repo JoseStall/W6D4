70449de7f25ff4930504907ea4100104926bf51d
bafraikin <fraikin.baptiste@gmail.com>
2018-05-10 18:03:20 +0200

    coucou guigui

--- Emailing/mailing.rb
+++ Emailing/mailing.rb
@@ -5,32 +5,35 @@ Dotenv.load('gmail.env')
 
 gmail = Gmail.connect!(ENV['username'],ENV['password'])
 
-email = []
+email = File.open('email_sort.csv', 'r')
+email = email.readlines
+email = email[0].split('\n')
 
 
-	gmail.deliver do 
-		to "g.delory62@gmail.com"
+email.each do | student |
+  gmail.deliver do 
+  to student
 
-subject "Présentation de The Hacking Project"
+  subject "Présentation de The Hacking Project"
 
-			body "Text of plaintext message."
-	
-			content_type 'text/html; charset=UTF-8'
-			body "<p>Bonjour,</p>
+  body "Text of plaintext message."
 
-			<p>	Nous sommes des élèves de <b>The Hacking Project.<br></b>
-			Une formation au code gratuite:
-			<ul>
-			<li>sans locaux</li>
-			<li>sans sélection</li>
-			<li>sans restriction géographique</li><br>
-			</ul>	
-			La pédagogie de notre formation est celle du <u>peer-learning</u>, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code.<br>
-			Aujourd'hui, on s'est fixé comme projet de créer une landing page pour pouvoir promouvoir cette formation, <br>
-			On a aussi décidé d'envoyer quelques mails aux anciens étudiants de lille pour leur présenter ce que des débutants savent faire en 1 mois et demi de code. <br>
-			Déjà 400 personnes sont passées par <b>The Hacking Project</b>et ont changé leur vie<br>
-			Est-ce ça te dirait de changer le monde avec nous ?<br>
-			Viens en découvrir plus sur cette <a href= 'https://hidden-sierra-91449.herokuapp.com/home/hackers' > adresse</a> . 
+  content_type 'text/html; charset=UTF-8'
+  body "<p>Bonjour,</p>
+                        <p>	Nous sommes des élèves de <b>The Hacking Project.<br></b>
+                        Une formation au code gratuite:
+                        <ul>
+                        <li>sans locaux</li>
+                        <li>sans sélection</li>
+                        <li>sans restriction géographique</li><br>
+                        </ul>	
+                        La pédagogie de notre formation est celle du <u>peer-learning</u>, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code.<br>
+                        Aujourd'hui, on s'est fixé comme projet de créer une landing page pour pouvoir promouvoir cette formation, <br>
+                        On a aussi décidé d'envoyer quelques mails aux anciens étudiants de lille pour leur présenter ce que des débutants savent faire en 1 mois et demi de code. <br>
+                        Déjà 400 personnes sont passées par <b>The Hacking Project</b>et ont changé leur vie<br>
+                        Est-ce ça te dirait de changer le monde avec nous ?<br>
+                        Viens en découvrir plus sur cette <a href= 'https://hidden-sierra-91449.herokuapp.com/home/hackers' > adresse</a> . 
 
-			<p>The Hacking Project Lille</p>"
-	end
+                        <p>The Hacking Project Lille</p>"
+  end
+end
