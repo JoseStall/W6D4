32d6dc63799076fd5bef29098eaf338ed4156e5a
guillaume <g.delory62@gmail.com>
2018-05-10 20:01:46 +0200

    Bot twitter

--- Twitter/bot2.rb
+++ Twitter/bot2.rb
@@ -5,8 +5,7 @@ Dotenv.load('script.env',)
 
 # quelques lignes qui enregistrent les clés d'APIs
 client = Twitter::REST::Client.new do |config|
-#client = Twitter::REST::Client.new do |config|
-#	client = Twitter::DirectMessage::Client.new do |config|
+
   config.consumer_key        = ENV['consumer_key']
   config.consumer_secret     = ENV['consumer_secret']
   config.access_token        = ENV['access_token']
@@ -15,5 +14,6 @@ end
 
 
 client.search("start-up -rt", lang: "fr").take(1).collect do |object|
-	client.update("@#{object.user.screen_name} Je te présente The Hacking Project, une formation au code en pair-programming qui pourra t'aider dans ton projet, retrouve nous sur ", in_reply_to_tweet_id: object.id )
+	client.update("@#{object.user.screen_name} Je te présente The Hacking Project, une formation au code en pair-programming qui pourra t'aider dans ton projet, retrouve nous sur https://thplille.herokuapp.com/hackers ", in_reply_to_tweet_id: object.id )
+sleep(120)
 end
