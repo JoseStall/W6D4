d1a98b6642d43ec3a8b5ee587e8961413b13c4bb
Gauthier Masse <masse.gauthier@gmail.com>
2018-05-17 13:29:56 +0200

    BDD qui marche

--- Landing_page/app/controllers/home_controller.rb
+++ Landing_page/app/controllers/home_controller.rb
@@ -1,24 +1,24 @@
 class HomeController < ApplicationController
   def hackers
-  	puts "yo"
-  	puts params[:email]
-  	@user = User.create(email: params[:email], newsletter: true)
   end
 
   def pros
-  	puts "yo"
-  	puts params[:email]
-  	@user = User.create(email: params[:email], newsletter: true)
   end
 
   def lille
-  	puts "yo"
-  	puts params[:email]
   	@user = User.create(email: params[:email], newsletter: true)
   end
 
   def ok
-  	@user = User.last
+    @user = User.create(email: params[:email], newsletter: true)
+    if @user.save
+        #redirect_to "/"
+        puts "yo"
+      else 
+        #redirect_to "hackers"
+        puts "yoyo"
+        flash.now[:error] = "Déjà inscrit!"
+      end
   end
 
 end
