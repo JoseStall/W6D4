a19b22d4a1a59e1b2004b43d7fad0223fa210fd1
guillaume <g.delory62@gmail.com>
2018-05-17 14:50:14 +0200

    implementation mailer

--- Landing_page/app/controllers/home_controller.rb
+++ Landing_page/app/controllers/home_controller.rb
@@ -6,19 +6,10 @@ class HomeController < ApplicationController
   end
 
   def lille
-  	@user = User.create(email: params[:email], newsletter: true)
   end
 
   def ok
     @user = User.create(email: params[:email], newsletter: true)
-    if @user.save
-        #redirect_to "/"
-        puts "yo"
-      else 
-        #redirect_to "hackers"
-        puts "yoyo"
-        flash.now[:error] = "Déjà inscrit!"
-      end
   end
 
 end
