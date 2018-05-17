3a2b17ef6f2f1eabc53aaaa61199ef42b15f4a14
guillaume <g.delory62@gmail.com>
2018-05-17 15:55:13 +0200

    newsletter

--- Landing_page/app/mailers/contact_mailer.rb
+++ Landing_page/app/mailers/contact_mailer.rb
@@ -1,3 +1,30 @@
 class ContactMailer < ApplicationMailer
+	def initialize
+		
+	end
+
+	def contact
+		@news = []
+		@users = User.where(newsletter: true)
+		@users.each do |user|
+		@news << {'Email'=> user.email,'Name'=> 'cool'}
+		end
+	Mailjet::Send.create(messages: [{
+				'From'=> {
+					'Email'=> 'thplille@gmail.com',
+					'Name'=> 'Voila'
+					},
+					'To'=> @news,
+						'Subject'=> 'Newsletter de The Hacking Project',
+						'TextPart'=> 'Voila',
+						'HTMLPart'=> "<h1>Newsletter The Hacking Project </h1>
+			<h2> Salut, comment ça va cette semaine? </h2>
+			<p>N'oublie pas d'aller checker le site <strong>de The Hacking Project </a></strong> pour t'inscrire à la prochaine session, apprendre à coder gratuitement en 3 mois et changer de vie!</p>
+
+			<p> Pour te désinscrire de la newsletter, .</p>"
+						}]
+						)
+
+	end
 
 end
--- Landing_page/app/services/weekly_newsletter.rb
+++ Landing_page/app/services/weekly_newsletter.rb
@@ -1,13 +1,12 @@
 class WeeklyNewsletter 
-
 	def perform
 		@users = User.where(newsletter: true)
 		news = []
 		@users.each do |user|
-		news << {'Email'=> user.email,'Name'=> 'cool'}
+			news << {'Email'=> user.email,'Name'=> 'cool'}
 		end
 
-	Mailjet::Send.create(messages:[{
+		Mailjet::Send.create(messages:[{
 			'From'=> {
 				'Email'=> 'thplille@gmail.com',
 				'Name'=> 'Guillaume'
@@ -16,14 +15,13 @@ class WeeklyNewsletter
 			'Subject'=> 'Newsletter de la bombe',
 			'TextPart'=> 'Voila ',
 			'HTMLPart'=> "<h1>Newsletter The Hacking Project </h1>
-<h2> Salut, comment ça va cette semaine? </h2>
-<p>N'oublie pas d'aller checker le site <strong>de The Hacking Project </a></strong> pour t'inscrire à la prochaine session, apprendre à coder gratuitement en 3 mois et changer de vie!</p>
+			<h2> Salut, comment ça va cette semaine? </h2>
+			<p>N'oublie pas d'aller checker le site <strong>de The Hacking Project </a></strong> pour t'inscrire à la prochaine session, apprendre à coder gratuitement en 3 mois et changer de vie!</p>
 
-<p> Pour te désinscrire de la newsletter, .</p>"
+			<p> Pour te désinscrire de la newsletter, .</p>"
 		}]
 		)
 	end
-
 end
 
 
